eb88e7dc7b728357b66a99502cf89175d45bf544
lohm5697 <lohm5697@PCB-LAB360-33>
2019-09-05 11:03:00 -0500

    Participation 1 on 9/5

---
+++ LoopParticipation1/LoopParticipation1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Jason Lohmer
+namespace LoopParticipation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            int sum = 0;
+
+            for (counter = 1; counter <= 100; counter++)
+            {
+                sum = sum + counter;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
