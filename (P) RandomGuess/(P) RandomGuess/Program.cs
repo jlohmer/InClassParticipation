6b7c07bc5a7fa81f300ce47a7255e80b7c73b829
lohm5697 <lohm5697@PCB-LAB360-33>
2019-09-10 11:41:44 -0500

    Participation done

---
+++ (P) RandomGuess/(P) RandomGuess/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Declare Variables
+            Random rand = new Random();
+            int randNumber = rand.Next(1, 100);
+            string userChoice;
+            int userNumber;
+
+            // Prompt user to guess the random number
+            Console.WriteLine("Guess my number! It's between 1 and 100!");
+            userChoice = Console.ReadLine();
+            userNumber = Convert.ToInt32(userChoice);
+            for (int i = 0; i < 100; i++)
+            {
+                if (userNumber == randNumber)
+                {
+                    Console.WriteLine("Congradulations! You guessed my number!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Nope, you're wrong! Guess again!\n");
+                    userChoice = Console.ReadLine();
+                    userNumber = Convert.ToInt32(userChoice);
+                }
+            }
+        }
+    }
+}
